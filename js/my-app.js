e0c18d67d87758c1c2f9310722d9dae5aa2a4d5a
Shamil Kaytmazov <altair@list.ru>
2017-09-26 20:49:43 +0300

    Added infinite scroll and bug fixed

--- js/my-app.js
+++ js/my-app.js
@@ -24,8 +24,9 @@ myApp.onPageInit('sign-in', function (page) {
   });
 });
 
-/* ===== Pull To Refresh ===== */
+/* ===== Project list ===== */
 myApp.onPageInit('projects-list', function (page) {
+  /* ===== Pull To Refresh ===== */
   var ptrContent = $$(page.container).find('.pull-to-refresh-content');
 
   ptrContent.on('refresh', function (e) {
@@ -35,6 +36,65 @@ myApp.onPageInit('projects-list', function (page) {
       myApp.pullToRefreshDone();
     }, 2000);
   });
+
+  /* ===== Infinite Scroll ===== */
+  // Loading flag
+  var loading = false;
+  // Last loaded index
+  var lastIndex = $$('.infinite-scroll .list-block li').length;
+  // Max items to load
+  var maxItems = 60;
+  // Append items per load
+  var itemsPerLoad = 5;
+  // Attach 'infinite' event handler
+  $$('.infinite-scroll').on('infinite', function () {
+   
+    // Exit, if loading in progress
+    if (loading) return;
+   
+    // Set loading flag
+    loading = true;
+   
+    // Emulate 1s loading
+    setTimeout(function () {
+      // Reset loading flag
+      loading = false;
+   
+      if (lastIndex >= maxItems) {
+        // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
+        myApp.detachInfiniteScroll($$('.infinite-scroll'));
+        // Remove preloader
+        $$('.infinite-scroll-preloader').remove();
+        return;
+      }
+   
+      // Generate new items HTML
+      var html = '';
+      for (var i = lastIndex + 1; i <= lastIndex + itemsPerLoad; i++) {
+        html += '<li>' + 
+                  '<a href="project.html" class="item-link item-content status-expires">' +
+                    '<div class="item-media">' +
+                      '<i class="material-icons projects-icon">access_time</i>' +
+                    '</div>' +
+                    '<div class="item-inner">' +
+                      '<div class="item-title-row">' +
+                        '<div class="item-title">Суши-смак ' + i + '</div>' +
+                        '<div class="item-after">70 руб.</div>' +
+                      '</div>' +
+                      '<div class="item-subtitle">г Махачкала, ул Ирчи Казака, д 35П</div>' +
+                      '<div class="item-text">02.09.2017 21:54</div>' +
+                    '</div>' +
+                  '</a>' +
+                '</li>';
+      }
+   
+      // Append new items
+      $$('.infinite-scroll .list-block ul').append(html);
+   
+      // Update last loaded index
+      lastIndex = $$('.infinite-scroll .list-block li').length;
+    }, 1000);
+  }); 
 });
 
 /* ===== Добавляем карту на страницу проекта ===== */
