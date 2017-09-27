35d3757c1f2567762024c83885a1a21fc7064a19
Shamil Kaytmazov <altair@list.ru>
2017-09-27 16:31:08 +0300

    new pages

--- js/my-app.js
+++ js/my-app.js
@@ -97,6 +97,109 @@ myApp.onPageInit('projects-list', function (page) {
   }); 
 });
 
+/* ===== Выберите категорию ===== */
+myApp.onPageInit('add-project-subcat', function (page) {
+  // Describe yourself picker
+  var pickerDescribe = myApp.picker({
+    input: '#ks-picker-describe',
+    rotateEffect: true,
+    cols: [
+      {
+        textAlign: 'left',
+        values: ('Super Lex Amazing Bat Iron Rocket Lex Cool Beautiful Wonderful Raining Happy Amazing Funny Cool Hot').split(' ')
+      },
+      {
+        values: ('Man Luthor Woman Boy Girl Person Cutie Babe Raccoon').split(' ')
+      },
+    ]
+  });
+
+  // Добавляем карту на страницу добавления проекта
+  var s=document.getElementsByTagName('script')[0]; 
+  var sc=document.createElement('script'); 
+
+  sc.type='text/javascript'; 
+  sc.async=false; 
+  sc.src='../js/map.js'; 
+  s.parentNode.insertBefore(sc,s);
+
+  // Inline date-time
+  var today = new Date();
+
+  var pickerInline = myApp.picker({
+    input: '#ks-picker-date',
+    container: '#ks-picker-date-container',
+    toolbar: false,
+    rotateEffect: true,
+    value: [today.getMonth(), today.getDate(), today.getFullYear(), today.getHours(), (today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes())],
+    onChange: function (picker, values, displayValues) {
+      var daysInMonth = new Date(picker.value[2], picker.value[0]*1 + 1, 0).getDate();
+      if (values[1] > daysInMonth) {
+        picker.cols[1].setValue(daysInMonth);
+      }
+    },
+    formatValue: function (p, values, displayValues) {
+      return displayValues[0] + ' ' + values[1] + ', ' + values[2] + ' ' + values[3] + ':' + values[4];
+    },
+    cols: [
+      // Months
+      {
+        values: ('0 1 2 3 4 5 6 7 8 9 10 11').split(' '),
+        displayValues: ('January February March April May June July August September October November December').split(' '),
+        textAlign: 'left'
+      },
+      // Days
+      {
+        values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+      },
+      // Years
+      {
+        values: (function () {
+          var arr = [];
+          for (var i = 1950; i <= 2030; i++) { arr.push(i); }
+          return arr;
+        })(),
+      },
+      // Space divider
+      {
+        divider: true,
+        content: '&nbsp;&nbsp;'
+      },
+      // Hours
+      {
+        values: (function () {
+          var arr = [];
+          for (var i = 0; i <= 23; i++) { arr.push(i); }
+          return arr;
+        })(),
+      },
+      // Divider
+      {
+        divider: true,
+        content: ':'
+      },
+      // Minutes
+      {
+        values: (function () {
+          var arr = [];
+          for (var i = 0; i <= 59; i++) { arr.push(i < 10 ? '0' + i : i); }
+          return arr;
+        })(),
+      }
+    ]
+  });
+
+  // Уведомление
+  $$('.notification-subcat').on('click', function () {
+    myApp.addNotification({
+      message: 'Поле должно быть заполнено: Когда нужно приступить к заданию',
+      button: {
+        text: 'Закрыть'
+      }
+    });
+  });
+});
+
 /* ===== Добавляем карту на страницу проекта ===== */
 myApp.onPageInit('project', function (page) { 
   var s=document.getElementsByTagName('script')[0]; 
